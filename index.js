400130013c75d08bb06475aed87f67592e40b062
Edmar Junior <edmarjunior17@gmail.com>
2020-05-08 11:42:23 -0300

    Correction in the generated readme path

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const program = require('commander');
 const pkg = require('./package.json')
 const exec = require('child_process').exec;
-const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs');
 
@@ -17,6 +16,7 @@ program.version(pkg.version)
   .option('-a, --auth [auth]', 'Create project with Auth module')
   .option('-c, --code [code]', 'Open project with Visual Studio Code')
   .action(function createApi(projectName, options) {
+    const pathNewProject = `${process.cwd()}\\${projectName}`;
 
     const repository = options.auth 
       ? 'https://github.com/edmarjunior/auth-api.git' 
@@ -36,29 +36,26 @@ program.version(pkg.version)
        * creating README.md 
        * */ 
       const modelReadme = options.auth ? authREADME : baseREADME;
-      const pathNewReadme = path.resolve(__dirname, projectName, 'README.md');
       
       const dataNewReadme = modelReadme
         .replace('[project_name]', projectName)
         .replace('[version]', pkg.version);
       
-      fs.writeFile(pathNewReadme, dataNewReadme, 'utf8', function (err) {
+      fs.writeFile(`${pathNewProject}\\README.md`, dataNewReadme, 'utf8', function (err) {
           if (err) return console.log(err);
       });
 
       /** 
        * executing too many commands
        * */
-      const pathNewProject = path.resolve(__dirname, projectName);
       const commands = `rm -r .git && git init && git add . && git commit -m "Initialize project using Edapi Create"${options.code ? ' && code .' : ''}`;
-
+      
       exec(commands, { cwd: pathNewProject }, function (err, stdout, stderr) {
         if(err) {
           console.log(chalk.red(stderr));
           return;
         }
       });
-
     });
   });
 
